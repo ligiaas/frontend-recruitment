8e35ccfa7323e9b22fcc0f2392e90ca8353ab9c6
ligiaas <ligiaalmeidasilva@gmail.com>
2020-06-30 23:55:02 -0300

    creating bag component

---
+++ src/components/bag/bag.styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const StyledBag = styled.div`
+  background-color: rgb(32, 32, 37);
+  padding: 50px 25px;
+  position: absolute;
+  right: 0;
+  top: 0px;
+  width: 793px;
+`
+
+export { StyledBag }
---
+++ src/components/bag/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../header'
+import { StyledBag } from './bag.styled'
+
+const Bag = () => {
+  return (
+    <StyledBag>
+      <Header />
+    </StyledBag>
+  )
+}
+
+export default Bag
