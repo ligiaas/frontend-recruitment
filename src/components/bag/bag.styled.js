1af2aa520f29573b36b40c0343db4f7caa4a30de
ligiaas <ligiaalmeidasilva@gmail.com>
2020-07-01 14:40:13 -0300

    adjustment bag size

--- src/components/bag/bag.styled.js
+++ src/components/bag/bag.styled.js
@@ -2,11 +2,12 @@ import styled from 'styled-components'
 
 const StyledBag = styled.div`
   background-color: rgb(32, 32, 37);
+  height: 100%;
   padding: 50px 25px;
   position: absolute;
   right: 0;
   top: 0px;
-  width: 793px;
+  width: ${props => props.width};
 `
 
 export { StyledBag }
--- src/components/bag/index.js
+++ src/components/bag/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import useWindowSize from '../../utils/use-window-size'
 import Header from '../header'
 import { StyledBag } from './bag.styled'
 
 const Bag = () => {
+  const screen = useWindowSize()
+  const size = screen.width - (screen.width * 0.20)
+  console.log(screen)
   return (
-    <StyledBag>
+    <StyledBag width={`${size}px`}>
       <Header number={0} />
     </StyledBag>
   )
