0d7395a795c9dcce3978d4fdccd211cf3e49acd1
ligiaas <ligiaalmeidasilva@gmail.com>
2020-07-02 20:25:08 -0300

    Creating StyledGrid comp

---
+++ src/components/StyledGrid.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const StyledGrid = styled.div`
+  align-items: flex-start;
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+`
+
+export { StyledGrid }
