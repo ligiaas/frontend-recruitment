ad8ae8b6fea675d3d0a5f2f852976b0ee09df28e
ligiaas <ligiaalmeidasilva@gmail.com>
2020-07-02 20:25:48 -0300

    Creating StyledImage comp

---
+++ src/components/StyledImage.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const StyledImage = styled.img`
+  height: ${props => props.height};
+  margin: 0;
+  width: ${props => props.width};
+`
+
+export { StyledImage }
