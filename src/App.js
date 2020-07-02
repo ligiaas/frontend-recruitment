2aeb9740b30abf8a8a58c04a6b736c7d416e8e11
ligiaas <ligiaalmeidasilva@gmail.com>
2020-07-02 20:30:10 -0300

    Add Grid comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import Bag from '../src/components/bag'
+import Bag from '../src/components/container/Bag'
+import Grid from '../src/components/container/Grid'
 
 function App() {
   return (
     <>
+      <Grid>
+        <h1 style={{color: '#000'}}>HOME</h1>
+      </Grid>
       <Bag />
     </>
   );
