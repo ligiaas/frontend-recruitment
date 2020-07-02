9c675650e6d81ea61b85e0a164c6aac75b26f90c
ligiaas <ligiaalmeidasilva@gmail.com>
2020-07-02 20:26:03 -0300

    Creating StyledParagraph comp

---
+++ src/components/StyledParagraph.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const StyledParagraph = styled.p`
+  color: ${props => props.color};
+  font-size: ${props => props.size};
+  line-height: 1.2;
+  text-align: ${props => props.align};
+`
+export { StyledParagraph }
