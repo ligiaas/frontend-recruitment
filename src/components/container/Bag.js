eda13da14269c6737d92e38f8a86447e4aae2d95
ligiaas <ligiaalmeidasilva@gmail.com>
2020-07-02 20:28:00 -0300

    Creating Bag container comp

--- src/components/container/Bag.js
+++ src/components/container/Bag.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import useWindowSize from '../../utils/use-window-size'
-import Header from '../header'
-import { StyledBag } from './bag.styled'
+import { StyledHeader } from '../StyledHeader'
+import { StyledBag } from '../StyledBag'
 
 const Bag = () => {
   const screen = useWindowSize()
   const size = screen.width - (screen.width * 0.20)
-  console.log(screen)
   return (
     <StyledBag width={`${size}px`}>
-      <Header number={0} />
+      <StyledHeader number={0} />
     </StyledBag>
   )
 }
