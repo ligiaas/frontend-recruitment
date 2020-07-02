08d75e9ab29cc846abcaff2188341f2bb7fed117
ligiaas <ligiaalmeidasilva@gmail.com>
2020-07-02 20:26:39 -0300

    Creating StyledProduct comp

---
+++ src/components/StyledProduct.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+const StyledProduct = styled.div`
+  align-items: center;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 20px 30px;
+`
+
+export { StyledProduct }
