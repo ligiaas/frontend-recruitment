f45c3feedd9f91a561ab6573606f714041945a30
ligiaas <ligiaalmeidasilva@gmail.com>
2020-07-05 10:48:25 -0300

    linking with redux

--- src/components/container/Grid.js
+++ src/components/container/Grid.js
@@ -1,55 +1,40 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-// import { fetchProducts } from '../../store/store';
 import PropTypes from 'prop-types'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchProducts } from '../../store/actions'
 import { StyledGrid } from '../StyledGrid'
 import { StyledImage } from '../StyledImage'
 import { StyledBar } from '../StyledBar'
 import { StyledParagraph } from '../StyledParagraph'
-import { StyledPrice } from '../StyledPrice'
+import Price from './Price'
 import Product from './Product'
-import Camisa from '../../assets/camisa-listra-pb.jpg'
+import Camisa from '../../assets/camisa-1.jpg'
 
 const Grid = () => {
+  const products = useSelector(state => state.products.products);
+  const loading = useSelector(state => state.loading);
+  const error = useSelector(state => state.error);
+  
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
 
-  const products = useSelector(
-    state => state.products,
-    products => products,
-  );
-  
-  const loading = useSelector(
-    state => state.loading,
-    loading => loading,
-  );
-  
-  const error = useSelector(
-    state => state.error,
-    error => error,
-  );
-  
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(fetchProducts());
-  // }, [dispatch]);
+  if (loading) { return 'loading...' }
 
-  if (loading) {
-    return (<StyledParagraph>loading...</StyledParagraph>)
-  }
-  
-  if (error) {
-    return (<StyledParagraph>ooops!</StyledParagraph>)
-  }
-  
+  if (error) { return 'ooops!' }
+
   return (
     <StyledGrid>
       {
+
         products.map(item => (
-          <Product key={item.id}>
+          <Product key={item.id} product={item}>
             <StyledImage atl={item.title} src={Camisa} />
             <StyledParagraph align='center'>{item.title}</StyledParagraph>
-            <StyledBar color='#dfbd00' height='13px' width='25px'></StyledBar>
+            <StyledBar color='#dfbd00' height='3px' width='15px'></StyledBar>
             <StyledParagraph align='center'>
-              R$ <StyledPrice>{item.price}</StyledPrice>
+              R$ <Price value={item.price} />
             </StyledParagraph>
           </Product>
         ))
