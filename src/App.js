5383a7017150486daa3d267e3264ddecac90459d
ligiaas <ligiaalmeidasilva@gmail.com>
2020-07-05 10:42:50 -0300

    add BagButton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Bag from '../src/components/container/Bag'
+import BagButton from '../src/components/BagButton'
 import Grid from '../src/components/container/Grid'
 
 function App() {
+  const [open, setOpen] = useState(false)
+
   return (
     <>
       <Grid>
         <h1 style={{color: '#000'}}>HOME</h1>
       </Grid>
-      <Bag />
+      <BagButton open={open} setOpen={setOpen} />
+      <Bag open={open} setOpen={setOpen} />
     </>
   );
 }
