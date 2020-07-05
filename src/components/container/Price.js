9762351b325bc83339e337e012fab5f160b1278c
ligiaas <ligiaalmeidasilva@gmail.com>
2020-07-05 10:47:35 -0300

    making layout adjustments

--- src/components/container/Price.js
+++ src/components/container/Price.js
@@ -2,22 +2,26 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StyledPrice } from '../StyledPrice'
 
-const Price = ({ children, color, size }) => {
-  
+const Price = ({ color, value }) => {
+  const str = value.toString()
+  const int = str.split('.')
+
   return (
-    <StyledPrice color={color} size={size}>
-      {children}
-    </StyledPrice>
+    <>
+      <StyledPrice color={color} size="22px" weight="bold">{int[0]}</StyledPrice>.
+      <StyledPrice>{int[1]}</StyledPrice>
+    </>
   )
 }
 
 Price.default = {
   color: '#313135',
-  size: '20px'
+  size: '16px',
+  weight: 'normal'
 }
 
 Price.Proptype = {
-  children: PropTypes.node.isRequired,
+  value: PropTypes.string.isRequired,
   color: PropTypes.string,
   size: PropTypes.string
 }
