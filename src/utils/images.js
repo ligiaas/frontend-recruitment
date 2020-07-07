8331a4bc120d0bad255e791263a983518af0aa9b
ligia_b_silva <ligia.b.silva@capgemini.com>
2020-07-06 21:49:00 -0300

    add images products and icons images

---
+++ src/utils/images.js
@@ -0,0 +1,37 @@
+import Camisa1 from '../assets/camisa-1.jpg';
+import Camisa2 from '../assets/camisa-2.jpg';
+import Camisa3 from '../assets/camisa-3.jpg';
+import Camisa4 from '../assets/camisa-4.jpg';
+import Camisa5 from '../assets/camisa-5.jpg';
+import Camisa6 from '../assets/camisa-6.jpg';
+import Camisa7 from '../assets/camisa-7.jpg';
+import Camisa8 from '../assets/camisa-8.jpg';
+import Camisa9 from '../assets/camisa-9.jpg';
+import Camisa10 from '../assets/camisa-10.jpg';
+import Camisa11 from '../assets/camisa-11.jpg';
+import Camisa12 from '../assets/camisa-12.jpg';
+import Camisa13 from '../assets/camisa-13.jpg';
+import Camisa14 from '../assets/camisa-14.jpg';
+import Camisa15 from '../assets/camisa-15.jpg';
+import Camisa16 from '../assets/camisa-16.jpg';
+import Camisa17 from '../assets/camisa-17.jpg';
+
+export const images = [
+  Camisa1,
+  Camisa2,
+  Camisa3,
+  Camisa4,
+  Camisa5,
+  Camisa6,
+  Camisa7,
+  Camisa8,
+  Camisa9,
+  Camisa10,
+  Camisa11,
+  Camisa12,
+  Camisa13,
+  Camisa14,
+  Camisa15,
+  Camisa16,
+  Camisa17
+]
