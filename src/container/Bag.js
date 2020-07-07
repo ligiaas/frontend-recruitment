00b69c6000e0535e232cc9c18ab530b3d44ef3a0
ligia_b_silva <ligia.b.silva@capgemini.com>
2020-07-07 01:23:21 -0300

    add functionality total and parcel total

--- src/container/Bag.js
+++ src/container/Bag.js
@@ -5,14 +5,17 @@ import Proptypes from 'prop-types';
 import useWindowSize from '../utils/use-window-size';
 import Header from '../components/Header';
 import { StyledBag } from '../ui/StyledBag';
-import Paragraph from '../components/Paragraph';
+import { StyledBagSubTotal, StyledSubTotalValue } from '../ui/StyledBagSubTotal';
+import { StyledButton } from '../ui/StyledButton';
 import BagItem from '../components/BagItem'
+import Paragraph from '../components/Paragraph';
 
 const Bag = ({ open }) => {
 
-  const [delected, setDelected] = useState(false)
   const screen = useWindowSize()
   const widthSize = screen.width - (screen.width * 0.4)
+  const [delected, setDelected] = useState(false)
+  const [total, updateTotal] = useState(0)
 
   const products = useSelector(state => state.bag.products)
   const loading = useSelector(state => state.loading);
@@ -34,15 +37,67 @@ const Bag = ({ open }) => {
     dispatch(bagRemoveProduct(product))
   }
 
+  const getTotal = () => {
+    const itemValue = products.map(item => (
+      item.product.price * item.amount
+    ))
+    return itemValue.reduce((amount, value) => amount + value, 0).toFixed(2)
+  }
+
+  const parcelTotal = () => {
+    const parcel = getTotal() / 10
+    return parcel.toFixed(2)
+  }
+
   return (
     <StyledBag open={open} height={screen.height} width={open ? widthSize : 0}>
       <Header counter={products.length} />
       {
         products && products.map((item, index) => (
-          <BagItem key={index} onClick={() => deleteProduct(item)} remove={delected} request={item} />
+          <BagItem
+            key={index}
+            onClick={() => deleteProduct(item)}
+            remove={delected}
+            request={item}
+          />
         ))
       }
-
+      <StyledBagSubTotal>
+        <Paragraph
+          color="rgba(255, 255, 255, 0.4)"
+          size={18}
+          transform="uppercase"
+        >
+          subtotal
+        </Paragraph>
+        <StyledSubTotalValue>
+          <Paragraph
+            color="#dfbd00"
+            size={28}
+            transform="uppercase"
+          >
+            r$ {getTotal()}
+          </Paragraph>
+          <Paragraph
+            color="rgba(255, 255, 255, 0.2)"
+            size={12}
+            transform="uppercase"
+          >
+            ou em até 10 x {parcelTotal()}
+          </Paragraph>
+        </StyledSubTotalValue>
+      </StyledBagSubTotal>
+      <StyledButton
+        bgcolor="#000"
+        radius={2}
+        boxshadow="0px 2px 0px 0px rgba(0, 0, 0, 0.2)"
+        color="#fff"
+        hvcolor="#dfbd00"
+        padding="10px 0"
+        width="100%"
+      >
+        <Paragraph align="center" transform="uppercase">comprar</Paragraph>
+      </StyledButton>
     </StyledBag>
   )
 }
